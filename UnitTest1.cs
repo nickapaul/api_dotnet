b625cf515412a23e85e108bba093c9e7539e3344
Nicolas Paul <nicolas.paul@outlook.com>
2023-10-03 21:52:23 -0400

    initial

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,10 +1,41 @@
 namespace api_dotnet;
+using RestSharp;
 
-public class UnitTest1
+public class Tests
 {
-    [Fact]
-    public void Test1()
+    [SetUp]
+    public void Setup()
     {
+    }
 
+    [Test]
+    public async Task Test1()
+    {
+        var options = new RestClientOptions("https://petstore.swagger.io"){MaxTimeout = -1,};
+        var client = new RestClient(options);
+        var request = new RestRequest("/v2/pet", Method.Post);
+        request.AddHeader("Content-Type", "application/json");
+        var body = @"{" + "\n" +
+        @"  ""id"": 0," + "\n" +
+        @"  ""category"": {" + "\n" +
+        @"    ""id"": 0," + "\n" +
+        @"    ""name"": ""string""" + "\n" +
+        @"  }," + "\n" +
+        @"  ""name"": ""doggie""," + "\n" +
+        @"  ""photoUrls"": [" + "\n" +
+        @"    ""string""" + "\n" +
+        @"  ]," + "\n" +
+        @"  ""tags"": [" + "\n" +
+        @"    {" + "\n" +
+        @"      ""id"": 0," + "\n" +
+        @"      ""name"": ""string""" + "\n" +
+        @"    }" + "\n" +
+        @"  ]," + "\n" +
+        @"  ""status"": ""available""" + "\n" +
+        @"}";
+        request.AddStringBody(body, DataFormat.Json);
+        RestResponse response = await client.ExecuteAsync(request);
+        Console.WriteLine(response.Content);
+        
     }
 }
